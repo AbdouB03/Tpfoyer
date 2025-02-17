e2577b4927ba62f79d13d21364ab6ba41149dcbf
AbdouB03 <abdoubouafif10@gmail.com>
2025-02-17 16:32:38 +0100

    first commit

---
+++ src/main/java/tn/esprit/tpfoyer/entity/Chambre.java
@@ -0,0 +1,49 @@
+import jakarta.persistence.*;
+
+@Entity
+public class Chambre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idChambre;
+
+    private Long numeroChambre;
+
+    @Enumerated(EnumType.STRING)
+    private TypeChambre typeC;
+
+    @ManyToOne
+    @JoinColumn(name = "idBloc")
+    private Bloc bloc;
+
+    public Long getIdChambre() {
+        return idChambre;
+    }
+
+    public void setIdChambre(Long idChambre) {
+        this.idChambre = idChambre;
+    }
+
+    public Long getNumeroChambre() {
+        return numeroChambre;
+    }
+
+    public void setNumeroChambre(Long numeroChambre) {
+        this.numeroChambre = numeroChambre;
+    }
+
+    public TypeChambre getTypeC() {
+        return typeC;
+    }
+
+    public void setTypeC(TypeChambre typeC) {
+        this.typeC = typeC;
+    }
+
+    public Bloc getBloc() {
+        return bloc;
+    }
+
+    public void setBloc(Bloc bloc) {
+        this.bloc = bloc;
+    }
+}
---
+++ src/main/java/tn/esprit/tpfoyer/entity/Reservation.java
@@ -0,0 +1,61 @@
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Reservation {
+    @Id
+    private String idReservation;
+
+    @Temporal(TemporalType.DATE)
+    private Date anneeUniversitaire;
+
+    private boolean estValide;
+
+    @ManyToOne
+    @JoinColumn(name = "idChambre")
+    private Chambre chambre;
+
+    @ManyToOne
+    @JoinColumn(name = "idEtudiant")
+    private Etudiant etudiant;
+
+    public String getIdReservation() {
+        return idReservation;
+    }
+
+    public void setIdReservation(String idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public Date getAnneeUniversitaire() {
+        return anneeUniversitaire;
+    }
+
+    public void setAnneeUniversitaire(Date anneeUniversitaire) {
+        this.anneeUniversitaire = anneeUniversitaire;
+    }
+
+    public boolean isEstValide() {
+        return estValide;
+    }
+
+    public void setEstValide(boolean estValide) {
+        this.estValide = estValide;
+    }
+
+    public Chambre getChambre() {
+        return chambre;
+    }
+
+    public void setChambre(Chambre chambre) {
+        this.chambre = chambre;
+    }
+
+    public Etudiant getEtudiant() {
+        return etudiant;
+    }
+
+    public void setEtudiant(Etudiant etudiant) {
+        this.etudiant = etudiant;
+    }
+}
